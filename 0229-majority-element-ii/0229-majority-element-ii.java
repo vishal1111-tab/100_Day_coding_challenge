9d11671bf34f0e789541f33bdd7d67c766170d8a
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-06-29 19:33:45 +0530

    Time: 3 ms (70.24%), Space: 48.5 MB (7.79%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,5 +1,5 @@
 class Solution {
-    public List<Integer> majorityElement(int[] v) {
+    public List<Integer> majorityElement(int[] nums) {
         
         // List <Integer> ans = new ArrayList<>();
         
@@ -21,22 +21,75 @@ class Solution {
            
         // }
         // return ans;
-  HashMap<Integer, Integer> mpp = new HashMap<>();
-  List<Integer> ls = new ArrayList<>();
-         int n = v.length;
+//   HashMap<Integer, Integer> mpp = new HashMap<>();
+//   List<Integer> ls = new ArrayList<>();
+//          int n = v.length;
  
-        int mini = (int)(n / 3) + 1;
+//         int mini = (int)(n / 3) + 1;
 
-        for (int i = 0; i < n; i++) {
-            int value = mpp.getOrDefault(v[i], 0);
-            mpp.put(v[i], value + 1);
+//         for (int i = 0; i < n; i++) {
+//             int value = mpp.getOrDefault(v[i], 0);
+//             mpp.put(v[i], value + 1);
 
-            if (mpp.get(v[i]) == mini) {
-                ls.add(v[i]);
-            }
-            if (ls.size() == 2) break;
+//             if (mpp.get(v[i]) == mini) {
+//                 ls.add(v[i]);
+//             }
+//             if (ls.size() == 2) break;
+//         }
+
+//         return ls;
+
+       
+       int count1 =0;
+       int ele1 = 0;
+        int count2 =0;
+       int ele2 = 0;
+        int mini = (int)(nums.length/3);
+       for(int i=0; i<nums.length; i++){
+        if(count1 == 0 && nums[i] != ele2){
+            count1 =1;
+            ele1 = nums[i];
         }
+        else if(count2 == 0 && nums[i] != ele1){
+            count2 =1;
+            ele2 = nums[i];
+        }
+        else if(nums[i] == ele1){
+            count1++;
+        }
+        else if(nums[i] == ele2){
+            count2++;
+        }
+        else{
+            count1--;
+            count2--;
+        }
+       }
+       
+       int c1 = 0;
+       int c2 = 0;
+       for(int i=0; i<nums.length; i++){
+         if(nums[i]==ele1){
+            c1++;
+         }
+         else if(nums[i]==ele2){
+            c2++;
+         }
+       }
+       
+       List <Integer> res = new ArrayList<>();
+       if(c1 > mini){
+         res.add(ele1);
+       }
+       if(c2 > mini){
+        res.add(ele2);
+       }
+       return res;
+       
+       
 
-        return ls;
+
+
+
     }
 }
