327071cf606b3e4b50abc7fa8d92695f7879b81d
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-06-29 18:18:46 +0530

    Time: 16 ms (10.51%), Space: 47.4 MB (81.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,25 +1,42 @@
 class Solution {
-    public List<Integer> majorityElement(int[] nums) {
+    public List<Integer> majorityElement(int[] v) {
         
-        List <Integer> ans = new ArrayList<>();
+        // List <Integer> ans = new ArrayList<>();
         
-        for(int i=0; i<nums.length; i++){
-            int count = 0;
-            if(ans.size()== 0 || ans.get(0) !=nums[i]){
-               for(int j=0; j<nums.length; j++){
-                if(nums[i]==nums[j]){
-                    count ++;
-                    if(count > (nums.length/3)){
-                    ans.add(nums[i]);
-                    break;
-                }
-                } 
-            }
+        // for(int i=0; i<nums.length; i++){
+        //     int count = 0;
+        //     if(ans.size()== 0 || ans.get(0) !=nums[i]){
+        //        for(int j=0; j<nums.length; j++){
+        //         if(nums[i]==nums[j]){
+        //             count ++;
+        //             if(count > (nums.length/3)){
+        //             ans.add(nums[i]);
+        //             break;
+        //         }
+        //         } 
+        //     }
                
-            }
-            if (ans.size() == 2) break;
+        //     }
+        //     if (ans.size() == 2) break;
            
+        // }
+        // return ans;
+  HashMap<Integer, Integer> mpp = new HashMap<>();
+  List<Integer> ls = new ArrayList<>();
+         int n = v.length;
+ 
+        int mini = (int)(n / 3) + 1;
+
+        for (int i = 0; i < n; i++) {
+            int value = mpp.getOrDefault(v[i], 0);
+            mpp.put(v[i], value + 1);
+
+            if (mpp.get(v[i]) == mini) {
+                ls.add(v[i]);
+            }
+            if (ls.size() == 2) break;
         }
-        return ans;
+
+        return ls;
     }
 }
