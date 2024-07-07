c991f7a9c851cc87e02222de6aa836341793df99
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-07-07 13:48:37 +0530

    Time: 1 ms (86.68%), Space: 40.5 MB (84.75%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,31 @@
 class Solution {
     public int mySqrt(int x) {
-        int res = 0;
-        for(long i=1; i*i<=x; i++){
-           res = (int)i;
+        // int res = 0;
+        // for(long i=1; i*i<=x; i++){
+        //    res = (int)i;
+        // }
+        // return res;
+
+        long lo =1; long hi = x;
+        long fans = 0;
+        
+        while(lo <=hi){
+          long mid = lo + (hi-lo)/2;
+         
+           long ans = mid*mid;
+           if(ans>x){
+            hi = mid-1;
+           }
+           else if(ans<x){
+            fans = mid;
+            lo = mid +1;
+           }
+           else{
+            fans = mid;
+            break;
+           }
+ 
         }
-        return res;
+        return (int)fans;
     }
 }
