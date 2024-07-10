770232a6b8cbe381532f4e668cecd32fc16825f1
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-07-10 14:27:26 +0530

    Time: 0 ms (100%), Space: 43.2 MB (11.73%) - LeetHub

--- 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/1646-kth-missing-positive-number.java
@@ -1,5 +1,7 @@
 class Solution {
     public int findKthPositive(int[] arr, int k) {
+
+        // brute force Tc => O(n) sc => O(n);
         // Set<Integer> hm = new HashSet<>();
 
         // for(int x : arr){
@@ -18,17 +20,43 @@ class Solution {
         // }
         // return -1;
         
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]<=k){
-                k++;
-            }
-            else{
-                break;
-            }
+        // Tc => O(n) sc => O(1);
+
+
+        // for(int i=0; i<arr.length; i++){
+        //     if(arr[i]<=k){
+        //         k++;
+        //     }
+        //     else{
+        //         break;
+        //     }
+        // }
+        // return k;
+
+
+        int lo = 0;
+        int hi = arr.length-1;
+        while(lo <= hi){
+          int mid = (lo+hi)/2;
+          int miss = arr[mid] - (mid + 1);
+
+          if(miss < k){
+             lo = mid +1;
+          }
+          else{
+            hi = mid-1;
+          }
         }
-        return k;
+         
+         //evaluate
+        // int miss2 = arr[hi] - (hi + 1);
+        // int more = k-miss2;
+        // int ans = arr[hi] + more;
 
-
+    //    int ans = hi+1+k; or int ans = lo +k;
+       
+       int ans = hi +1 +k;
+        return ans;
 
     }
 }
