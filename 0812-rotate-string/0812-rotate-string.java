cfcb0829e7b2791f566dc7db72a764b941d2178b
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-07-13 00:14:11 +0530

    Time: 0 ms (100%), Space: 41.6 MB (18.78%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,9 +1,13 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+     
         if (s.length() != goal.length()) {
             return false;
         }
-        String doubledS = s + s;
-        return doubledS.contains(goal);
+        
+    
+        String doubledGoal = goal + goal;
+        
+        return doubledGoal.contains(s);
     }
 }
