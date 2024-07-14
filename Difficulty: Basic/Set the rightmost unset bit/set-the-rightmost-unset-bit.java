b13921fb97423baf76e2f8a1ec7f0119433cc627
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-07-15 03:26:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Set the rightmost unset bit/set-the-rightmost-unset-bit.java
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int N = sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans = ob.setBit(N);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static int setBit(int n) {
+        // code here
+         int countZero = 0;
+        int i = 0;
+        int ans = 0;
+
+        while (n != 0) {
+            int rem = n % 2;
+            if (countZero == 0 && rem == 0) {
+                ans = ans + (int)Math.pow(2, i);
+                countZero++;
+            } else {
+                ans = ans + (rem * (int)Math.pow(2, i));
+            }
+            i++;
+            n = n / 2;
+        }
+        if (countZero == 0) {
+            ans = ans + (int)Math.pow(2, i);
+        }
+        return ans;
+    }
+}
