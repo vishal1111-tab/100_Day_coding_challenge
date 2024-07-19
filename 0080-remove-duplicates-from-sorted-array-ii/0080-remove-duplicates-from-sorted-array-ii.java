0b13319869da8201ca5c855087933b4a27c571b4
Vishal Kumar <88841784+vishal1111-tab@users.noreply.github.com>
2024-07-19 15:32:47 +0530

    Time: 0 ms (100%), Space: 44.4 MB (36.67%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,30 +1,40 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        
-      HashMap<Integer, Integer> hm = new HashMap<>();
+    //   HashMap<Integer, Integer> hm = new HashMap<>();
+    //     for(int i =0; i< nums.length; i++){
+    //         int val = hm.getOrDefault(nums[i] , 0);
+    //         if(val < 2){
+    //           hm.put(nums[i] , val+1);
+    //         }
+    //     }
 
-        for(int i =0; i< nums.length; i++){
-            int val = hm.getOrDefault(nums[i] , 0);
-            if(val < 2){
-              hm.put(nums[i] , val+1);
-            }
-        }
-
-        List <Integer> list = new ArrayList<>();
+    //     List <Integer> list = new ArrayList<>();
         
-        for(int x : hm.keySet()){
-            int val2 = hm.get(x);
-            for(int i=0; i<val2; i++){
-                list.add(x);
-            }
-        }
-        Collections.sort(list);
+    //     for(int x : hm.keySet()){
+    //         int val2 = hm.get(x);
+    //         for(int i=0; i<val2; i++){
+    //             list.add(x);
+    //         }
+    //     }
+    //     Collections.sort(list);
         
        
-        for(int i=0; i< list.size(); i++){
-            nums[i] = list.get(i);
-        }
+    //     for(int i=0; i< list.size(); i++){
+    //         nums[i] = list.get(i);
+    //     }
         
-        return list.size();
+    //     return list.size();
+int i=2;
+for(int j=2;j<nums.length;j++){
+    if(nums[j]>nums[i-2]){
+        nums[i]=nums[j];
+        i++;
+    }
+  
+
+}
+return i;
+
+
     }
 }
